4974b0513534899642c7fcd657da61d89afc0025
Sliverwing <tlv591208510@gmail.com>
2017-01-30 21:04:33 +0800

    add App model & migration

---
+++ database/migrations/2017_01_30_115510_create_apps_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAppsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apps', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('to_id');
+            $table->string('token', 100)->unique();
+            $table->enum('notify_type', [
+                'alert',
+                'quiet',
+                'no'
+            ])->default('alert');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apps');
+    }
+}
